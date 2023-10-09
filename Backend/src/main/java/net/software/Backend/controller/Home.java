eb198a7cc0c046605dad536accfbed8505d8f646
Fennggg <fengguan@usc.edu>
2023-10-09 15:18:09 -0700

    add user model, refactor to restcontroller, setup postman to test api

---
+++ Backend/src/main/java/net/software/Backend/controller/Home.java
@@ -0,0 +1,25 @@
+package net.software.Backend.controller;
+
+import net.software.Backend.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+    @RequestMapping("/")
+    public String home(){
+        return "Home";
+    }
+
+    //访问user操作
+    @RequestMapping("/user")
+    public User user(){
+        User user = new User();
+        user.setId("3100");
+        user.setName("software");
+        user.setEmail("info@software.net");
+        return user;
+    }
+}
