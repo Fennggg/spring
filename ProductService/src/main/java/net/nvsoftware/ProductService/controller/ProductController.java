442bce615869f5ef6646d418a0609ff544071f7d
Fennggg <fengguan@usc.edu>
2023-10-10 18:08:48 -0700

    Product Service update

---
+++ ProductService/src/main/java/net/nvsoftware/ProductService/controller/ProductController.java
@@ -0,0 +1,33 @@
+package net.nvsoftware.ProductService.controller;
+
+import net.nvsoftware.ProductService.model.ProductRequest;
+import net.nvsoftware.ProductService.model.ProductResponse;
+import net.nvsoftware.ProductService.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+    @PostMapping
+    public ResponseEntity<Long> addProduct(@RequestBody ProductRequest productRequest) {
+        long id = productService.addProduct(productRequest);
+        return new ResponseEntity<>(id, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProductResponseById(@PathVariable long id) {
+        ProductResponse productResponse = productService.getById(id);
+        return new ResponseEntity<>(productResponse, HttpStatus.OK);
+    }
+
+    @PutMapping("/reduceQuantity")
+    public ResponseEntity<Void> reduceQuantity(@RequestParam long id, @RequestParam long quantity) {
+        productService.reduceQuantity(id, quantity);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
---
+++ ProductService/src/main/java/net/nvsoftware/ProductService/service/ProductService.java
@@ -0,0 +1,12 @@
+package net.nvsoftware.ProductService.service;
+
+import net.nvsoftware.ProductService.model.ProductRequest;
+import net.nvsoftware.ProductService.model.ProductResponse;
+
+public interface ProductService {
+    long addProduct(ProductRequest productRequest);
+
+    ProductResponse getById(long id);
+
+    void reduceQuantity(long id, long quantity);
+}
